2b62d30dd1e79568a050e4d58c3971f3ee39cd80
Seno Agung Pradityo <senoagung27@yahoo.com>
2022-03-11 11:49:39 +0700

    first app

---
+++ app/Models/Food.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Food extends Model
+{
+    use HasFactory , SoftDeletes;
+
+    protected $fillable = [
+        'name', 'description', 'ingredients', 'price', 
+        'rate', 'type', 'picturePath'
+    ];
+
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->timestamp;
+    }
+    public function getUpdatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->timestamp;
+    }
+
+    public function toArray()
+    {
+        $toArray = parent::toArray();
+        $toArray['picturePath'] = $this->picturePath;
+        return $toArray;
+    }
+
+    public function getPicturePathAtAttribute($value)
+    {
+        return url('') . Storage::url($this->attributes['picturePath']);
+    }
+}
